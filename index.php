92384c62b3287c30c7b1f32ec136e8137a4c1c10
Igor Macevic <64796141+Midbeast@users.noreply.github.com>
2021-11-24 17:47:19 +0100

    build a function to always redirect

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 
+use JetBrains\PhpStorm\NoReturn;
+
 $requestMethod = $_SERVER['REQUEST_METHOD'] ?? 'GET';
 $requestPath = $_SERVER['REQUEST_URI'] ?? '/';
 
@@ -10,10 +12,19 @@ $requestPath = $_SERVER['REQUEST_URI'] ?? '/';
  }
  else if($requestPath === '/old-home')
  {
-     header('Location: /', $replace = true, $code = 301);
-     exit;
+     redirectForeverTo('/');
+ }
+ else if($requestPath === '/test')
+ {
+     redirectForeverTo('/');
  }
  else
  {
      include(__DIR__ . '/includes/404.php');
  }
+
+#[NoReturn] 
+function redirectForeverTo($path) {
+    header("Location: {$path}", $replace = true, $code = 301);
+    exit;
+}
