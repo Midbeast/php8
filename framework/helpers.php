cbe3592e3d5254becd71b6f09e8158d28500dcd7
Igor Macevic <64796141+Midbeast@users.noreply.github.com>
2021-12-01 17:49:50 +0100

    Building the PHP-in-HTML Engine

--- framework/helpers.php
+++ framework/helpers.php
@@ -8,13 +8,14 @@ if (!function_exists('view')) {
         static $manager;
         if (!$manager) {
             $manager = new View\Manager();
-                // let's add a path for our views folder
-                // so the manager knows where to look for views
+            // let's add a path for our views folder
+            // so the manager knows where to look for views
             $manager->addPath(__DIR__ . '/../resources/views');
-                // we'll also start adding new engine classes
-                // with their expected extensions to be able to pick
-                // the appropriate engine for the template
+            // we'll also start adding new engine classes
+            // with their expected extensions to be able to pick
+            // the appropriate engine for the template
             $manager->addEngine('basic.php', new View\Engine\BasicEngine());
+            $manager->addEngine('php', new View\Engine\PhpEngine());
         }
         return $manager->render($template, $data);
     }
