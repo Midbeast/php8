29c228ed41f13e2964ec88ab5a9f790b78b247d6
Igor Macevic <64796141+Midbeast@users.noreply.github.com>
2022-01-06 21:48:26 +0100

    Installed Symfony Console

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInitd15b4dad46b0d8358b4d2f2a46a65fa8
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitd15b4dad46b0d8358b4d2f2a46a65fa8', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitd15b4dad46b0d8358b4d2f2a46a65fa8', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitd15b4dad46b0d8358b4d2f2a46a65fa8::getInitializer($loader));
         } else {
