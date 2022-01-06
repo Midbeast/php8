8ee612edfe767308823b9b2f7b5f48f99f8ad4ea
Igor Macevic <64796141+Midbeast@users.noreply.github.com>
2022-01-06 16:03:14 +0100

    Minor fix

--- app/Http/Controllers/ShowHomePageController.php
+++ app/Http/Controllers/ShowHomePageController.php
@@ -16,20 +16,20 @@ class ShowHomePageController
             return new MysqlConnection($config);
         });
 
-        $factory->addConnector('sqlite', function($config) {
-            return new SqliteConnection($config);
-        });
-
-        $config = require __DIR__ . '/../../../config/database.php';
-
-        $connection = $factory->connect($config[$config['default']]);
+        $connection = $factory->connect([
+            'type' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'pro-php-mvc',
+            'username' => 'root',
+            'password' => '',
+        ]);
 
         $product = $connection
             ->query()
             ->select()
             ->from('products')
             ->first();
-
         return view('home', [
             'number' => 42,
             'featured' => $product,
